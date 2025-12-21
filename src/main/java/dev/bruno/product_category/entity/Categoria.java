3e56ec3aa3b4b06235c3bf1a8fea9ef5ee662472
Bruno Barbosa <br.barbosa.souza1@gmail.com>
2025-12-21 08:05:56 -0300

    feat: adicionado as entidades Produto e Categoria com os relacionamentos ManyToOne e OneToMany

---
+++ src/main/java/dev/bruno/product_category/entity/Categoria.java
@@ -0,0 +1,53 @@
+package dev.bruno.product_category.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    @JsonIgnore
+    private List<Produto> produtos;
+
+    public Categoria() {}
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
---
+++ src/main/java/dev/bruno/product_category/entity/Produto.java
@@ -0,0 +1,63 @@
+package dev.bruno.product_category.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Produto() {}
+
+    public Produto(Long id, String nome, Double preco, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(Double preco){
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+}
